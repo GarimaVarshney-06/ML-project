8982d2e10fd865e32812f87506127fec57431488
Garima Varshney <gungunvarshney00@gmail.com>
2024-06-04 18:23:24 +0530

    data_transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,6 +5,8 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 #decorators
 @dataclass
 class DataIngestionConfig:
@@ -39,5 +41,7 @@ class DataIngestation:
         
 if __name__ == '__main__':
     obj=DataIngestation()
-    obj.initiate_data_ingestation()
+    train_data,test_data=obj.initiate_data_ingestation()
+    data_transformation =DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
     
